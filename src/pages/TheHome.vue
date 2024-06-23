3d11e911097cda1df3bae553476b4bcd2b04d1f5
Tiago Borges <tiagoborges899@gmail.com>
2024-06-23 10:10:33 -0300

    feat: criando request aos dados do json-sever e realizando busca no input por nome ou cpf/cnpj

--- src/pages/TheHome.vue
+++ src/pages/TheHome.vue
@@ -1,43 +1,67 @@
 <script setup>
-import TheButton from '../components/TheButton.vue'
-import axios from 'axios'
+import TheButton from "../components/TheButton.vue";
+import axios from "axios";
+import SearchIcon from '../components/icons/search-521.vue'
+
 </script>
 
 <script>
 export default {
+  data() {
+    return {
+      suppliers: [],
+      allSupliers : [],
+      response: "",
+      search: ""
+    };
+  },
+
   mounted() {
-            axios({ method: "GET", "url": "https://jsonplaceholder.typicode.com/todos" }).then(result => {
-               console.log(result)
-            }, error => {
-                console.error(error);
-            });
-        },
-}
+    axios.get("http://localhost:3000/fornecedores").then((response) => {
+      this.suppliers = response.data;
+      this.allSupliers = response.data
+    });
+  },
+
+  methods: {
+    searchByNameOrCpfCnpj() {
+      axios.get("http://localhost:3000/fornecedores")
+        .then(res => {
+          if (this.search) {
+            this.allSupliers = res.data.filter(people =>
+              people.empresa.toLowerCase().includes(this.search.toLowerCase())
+            );
+          } 
+        });
+    },
+
+    
+  },
+  created() {
+    this.searchByNameOrCpfCnpj();
+  }
+  
+};
 </script>
 
 <template>
+  <div class="header-container-input">
+    <SearchIcon class="search-icon" #icon />
+    <input v-model="search" type="text" class="search" placeholder="Busque por nome ou cpf" @input="searchByNameOrCpfCnpj()"/>
+  </div>
   <table>
     <h1>Conheça todos os nossos fornecedores</h1>
     <thead class="table-header">
       <tr>
         <td>Empresa</td>
-        <td>CNPJ / CPF</td>        
+        <td>CNPJ / CPF</td>
         <td>Açoes</td>
-
       </tr>
     </thead>
-    <tbody>
+    <tbody v-for="supplier in allSupliers" :key="supplier.id">
       <tr>
-        <td>Empresa fantasia</td>
-        <td>66.298.251/0001-02</td>
-        <td class="table-btn-actions">
-          <TheButton class="btn-edit">edit</TheButton>
-          <TheButton class="btn-remove">x</TheButton>
-        </td>
-      </tr>
-      <tr>
-        <td>Empresa fantasia</td>
-        <td>66.298.251/0001-02</td>
+        <td>{{ supplier.empresa }}</td>
+        <td>{{ supplier.cnpj }}</td>
         <td class="table-btn-actions">
           <TheButton class="btn-edit">edit</TheButton>
           <TheButton class="btn-remove">x</TheButton>
@@ -48,11 +72,36 @@ export default {
 </template>
 
 <style scoped>
+
+.header-container-input {
+  width: 80%;
+  display: flex;
+  justify-content: center;
+  position: relative;
+  bottom: 50px;
+}
+
+.search {
+  padding: .8rem 0;
+  width: 100%;
+  background-color: #eff5fb;
+  border: 0;
+  border-radius: 5px;
+  padding-left: 1.8rem;
+}
+
+
+.header-container-input > .search-icon {
+  position: absolute;
+  left: 8px;
+  top: 10px;
+}
+
 h1 {
-    text-align: center;
-    color: #d70000;
-    font-size: 1.2rem;
-    padding: 1.5rem 0;
+  text-align: center;
+  color: #d70000;
+  font-size: 1.2rem;
+  padding: 1.5rem 0;
 }
 
 table {
@@ -62,13 +111,13 @@ table {
 }
 
 table thead {
-    background-color: #d70000;
-    color: #FFF;
+  background-color: #d70000;
+  color: #fff;
 }
 
 table tbody {
-    background-color: #eff5fb;
-    color: #8c8181;
+  background-color: #eff5fb;
+  color: #8c8181;
 }
 
 table thead,
@@ -78,17 +127,16 @@ table tbody {
 
 table thead tr,
 table tbody tr {
-    width: 100%;
-    display: flex;
-    padding: 1.2rem 0;
-    justify-content: space-between;
-
+  width: 100%;
+  display: flex;
+  padding: 1.2rem 0;
+  justify-content: space-between;
 }
 
 table thead tr td,
 table tbody tr td {
-    width: 33.33%;
-    text-align: center;
+  width: 33.33%;
+  text-align: center;
 }
 
 .table-btn-actions {
